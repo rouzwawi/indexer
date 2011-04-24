fe149586d88b6b9f70ff86b1a9cccfee2c493524
Rouzbeh Delavari <rouzwawi@gmail.com>
2011-04-24 15:28:15 +0200

    page, region and file address converters

--- headers/mmf.hpp
+++ headers/mmf.hpp
@@ -2,22 +2,34 @@
 #include <boost/interprocess/mapped_region.hpp>
 
 #include <map>
+#include <algorithm>
 
 #include "typedefs.hpp"
 
 #ifndef MMF_H
 #define MMF_H
 
+#define RAMP_FILES 7
+#define PAGE_SIZE 4096
 #define PAGES_PER_REGION 2048
-#define REGIONS_PER_FILE 256
+
+
+/**********************************************
+ * sizes    as size_t
+ * adresses as char*
+ * pages    as u4
+ * regions  as u4
+ * files    as int
+ **********************************************/
 
 
 using namespace boost::interprocess;
+using namespace std;
 
 class mmf {
 
 private: // fields
-	std::map<u4, mapped_region*> regions;	// region_address -> region
+	map<u4, mapped_region*>      regions;	// region_address -> region
 	const file_mapping&          file_m;
 
 public:
@@ -28,14 +40,20 @@ public:
 	char* get_page(u4 page);
 	void flush(char* page);
 
-	static std::size_t page_size()   { return mapped_region::get_page_size();      }
-	static std::size_t region_size() { return PAGES_PER_REGION * mmf::page_size(); }
+	static size_t page_size()             { return PAGE_SIZE; }
+	static size_t region_size()           { return PAGES_PER_REGION * page_size(); }
+	static size_t file_size(int file)     { return file_regions(file) * region_size(); }
+	
+	static u4     file_regions(int file)  { return 1 << min(RAMP_FILES, file); }
+	static u4     regions_up_to(int file) { return file_regions(min(RAMP_FILES, file)) - 1 + max(0,file-RAMP_FILES) * file_regions(RAMP_FILES); }
+
+	static u4     region_addr(u4 page)    { return page / PAGES_PER_REGION; }
+	static int    file_addr(u4 region)    { for (int f=0;1;f++) if (regions_up_to(f) > region) return f-1; }
+	
 
 private:
 	bool is_region_mapped(u4 region_addr);
 	void map_region(u4 region_addr);
-
-	u4 region_addr(u4 page) { return page / PAGES_PER_REGION; }
 
 };
 
--- test.cpp
+++ test.cpp
@@ -33,9 +33,67 @@ void wah()
 	std::cout << WAH::fillvl(0xCFFFFFFFFFFFFFFF) << std::endl;
 }
 
+int test_addr_calcs()
+{
+	assert(0x00001000 == mmf::page_size());
+	assert(0x00800000 == mmf::region_size());
+
+	assert(0x00800000 == mmf::file_size(0));
+	assert(0x01000000 == mmf::file_size(1));
+	assert(0x02000000 == mmf::file_size(2));
+	assert(0x04000000 == mmf::file_size(3));
+	assert(0x08000000 == mmf::file_size(4));
+	assert(0x10000000 == mmf::file_size(5));
+	assert(0x20000000 == mmf::file_size(6));
+	assert(0x40000000 == mmf::file_size(7));
+	assert(0x40000000 == mmf::file_size(8));
+
+	assert(  1 == mmf::file_regions(0));
+	assert(  2 == mmf::file_regions(1));
+	assert(  4 == mmf::file_regions(2));
+	assert(  8 == mmf::file_regions(3));
+	assert( 16 == mmf::file_regions(4));
+	assert( 32 == mmf::file_regions(5));
+	assert( 64 == mmf::file_regions(6));
+	assert(128 == mmf::file_regions(7));
+	assert(128 == mmf::file_regions(8));
+
+	assert(  0 == mmf::regions_up_to(0));
+	assert(  1 == mmf::regions_up_to(1));
+	assert(  3 == mmf::regions_up_to(2));
+	assert(  7 == mmf::regions_up_to(3));
+	assert( 15 == mmf::regions_up_to(4));
+	assert( 31 == mmf::regions_up_to(5));
+	assert( 63 == mmf::regions_up_to(6));
+	assert(127 == mmf::regions_up_to(7));
+	assert(255 == mmf::regions_up_to(8));
+
+	assert( 0 == mmf::file_addr(  0));
+	assert( 1 == mmf::file_addr(  1));
+	assert( 1 == mmf::file_addr(  2));
+	assert( 2 == mmf::file_addr(  3));
+	assert( 2 == mmf::file_addr(  6));
+	assert( 3 == mmf::file_addr(  7));
+	assert( 3 == mmf::file_addr( 14));
+	assert( 4 == mmf::file_addr( 15));
+	assert( 4 == mmf::file_addr( 30));
+	assert( 5 == mmf::file_addr( 31));
+	assert( 5 == mmf::file_addr( 62));
+	assert( 6 == mmf::file_addr( 63));
+	assert( 6 == mmf::file_addr(126));
+	assert( 7 == mmf::file_addr(127));
+	assert( 7 == mmf::file_addr(254));
+	assert( 8 == mmf::file_addr(255));
+	assert( 8 == mmf::file_addr(382));
+	assert( 9 == mmf::file_addr(383));
+
+	return 0;
+}
+
 int main(int argc, const char* argv[])
 {
-	cout << "page_size " << mmf::page_size() << endl;
+	test_addr_calcs();
+	
 	
 	file_mapping data_file(argv[1], read_write);
 	
@@ -45,13 +103,16 @@ int main(int argc, const char* argv[])
 	//bitmap bitmap(data_file, index_page_0);
 	
 	string x;
-	do {
+	while (cin >> x) {
+		if (x == "q") exit(0);
+		
 		//bitmap.foo();
-		cin >> x;
 		u4 page = lexical_cast<u4>(x);
-		int* p = (int*) f.get_page(page);
-		cout << hex << p[0] << endl;
-	} while (x != "q");
+		//int* p = (int*) f.get_page(page);
+		//cout << hex << p[0] << endl;
+		
+		cout << "page " << page << " region " << mmf::region_addr(page) << " file " << mmf::file_addr(mmf::region_addr(page)) << endl;
+	}
 	
 	return 0;
 }
