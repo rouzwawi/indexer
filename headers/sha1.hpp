9d1e0f0a83f8eb7d73d4d80d036f13fec30dd0d1
Rouzbeh Delavari <rouzwawi@gmail.com>
2011-04-26 22:24:08 +0200

    some const fixings

--- headers/sha1.hpp
+++ headers/sha1.hpp
@@ -11,18 +11,18 @@ class sha1
 public:
 	typedef unsigned int(&sha1_type)[5];
 	
-	static void digest(char const* str, sha1_type digest) {
+	static void digest(const char* str, sha1_type digest) {
 		boost::uuids::detail::sha1 s;
 		s.process_bytes(str, strlen(str));
 		s.get_digest(digest);
 	}
 
-	static bool same(const sha1_type digest1, const sha1_type digest2) {
+	static bool same(sha1_type digest1, sha1_type digest2) {
 		for (int i=0; i<5; i++) if (digest1[i] != digest2[i]) return false;
 		return true;
 	}
 
-	static void copy(sha1_type to, const sha1_type from) {
+	static void copy(sha1_type to, sha1_type from) {
 		memcpy(to, from, sizeof(sha1_type));
 	}
 
